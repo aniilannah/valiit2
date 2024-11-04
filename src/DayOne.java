d16dfbe69117c4b7cff712042aad51ddf845d3fc
aniilannah <hannaliina.m@gmail.com>
2024-11-04 14:50:11 +0200

    Hello

--- src/DayOne.java
+++ src/DayOne.java
@@ -2,8 +2,27 @@
 public class DayOne {
     public static void main(String[] args) {
 
-        System.out.println("Hanna-Liina");
+        // System.out.println("Hanna-Liina");
 
-        // Task 1
+        //If, else if ja else
+
+        int a = 1;
+        int b = 2;
+
+        // TODO - print out "b is bigger than a"
+        // TODO -print out "a is bigger than b" in else if block
+        // TODO - print out "a and b are equal" in else block
+
+        if (b>a) {
+            System.out.println("b is bigger than a");
+        }
+        else if (a>b) {
+            System.out.println("a is bigger than b");
+        }
+        else {
+            System.out.println("a and b are equal");
+        }
+
+        //
     }
 }
