f806ce9b3a9c63e51d6586f05c6f08b4872cbe3f
aniilannah <hannaliina.m@gmail.com>
2024-11-08 09:24:17 +0200

    for each loop

---
+++ src/DayFour/ForEachLoop.java
@@ -0,0 +1,13 @@
+package DayFour;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        String [] fruits = {"Apple", "Banana", "Orange", "Grape"};
+        for(String fruit : fruits) {
+            System.out.println(fruit);
+        }
+
+
+
+    }
+}
