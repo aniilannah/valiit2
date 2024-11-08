ce3370aee8231df498af418d84727f5965ace416
aniilannah <hannaliina.m@gmail.com>
2024-11-08 09:23:13 +0200

    return exercises

---
+++ src/DayThree/ReturnMethodExercise2.java
@@ -0,0 +1,32 @@
+package DayThree;
+
+public class ReturnMethodExercise2 {
+    public static void main(String[] args) {
+
+        //boolean checkIfNumberISEven = isEven(4); - esimene näide
+        //System.out.println(checkIfNumberISEven); - esimene näide
+
+        // int userID = 1; - teine näide
+        // String username = getUSerFullName(userID); - teine näide
+    }
+
+    //private static boolean isEven(int number ) {
+      //  return number % 2 == 0;
+    //} - esimene näide
+
+    private static void getUser() {
+
+    }
+ private static String getUSerFullName (String userID) {
+        String userFullName = "John Doe";
+        return userFullName;
+    }
+    private static int getUserAge (int userID) {
+        int age = 30;
+        return age;
+    }
+
+
+
+
+}
